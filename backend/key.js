777412062a7e2510bc9c9dc6427e4bfda5b203d1
Vishal Jaiswal <88589080+dominatorVJ@users.noreply.github.com>
2023-07-07 08:05:08 +0530

    Delete key.js

--- backend/key.js
+++
@@ -1,5 +0,0 @@
-const JWT_KEY = "4rgsgdffhrfdggete5575986789thgf";
-const pass = "oowecvgfgtmyvlcf";
-const db_link =
-  "mongodb+srv://1vishaljee:1vishaljee@cluster0.yrcgkie.mongodb.net/?retryWrites=true&w=majority";
-module.exports = { JWT_KEY, pass, db_link };
