053470b11018426678b62b8e65eb8862722a1f4f
dominatorVJ <1vishaljee@gmail.com>
2023-07-07 23:37:55 +0530

    Updated

--- backend/key.js
+++ backend/key.js
@@ -3,4 +3,6 @@ const pass = "oowecvgfgtmyvlcf";
 const db_link =
   "mongodb+srv://1vishaljee:1vishaljee@cluster0.yrcgkie.mongodb.net/?retryWrites=true&w=majority";
   const flightAPI = "ae65c6c19fmsh43997907fbf7f73p171289jsn5568b4f90170";
-module.exports = { JWT_KEY, pass, db_link , flightAPI};
+  const trainAPI = "c7f01c3b62msh8fa8a769a9bb44bp1e60d9jsn05992acbe3d7";
+  const hotelAPI = "c7f01c3b62msh8fa8a769a9bb44bp1e60d9jsn05992acbe3d7";
+module.exports = { JWT_KEY, pass, db_link , flightAPI, trainAPI, hotelAPI};
