4e61793d0396bdcfd6c893c8559d7be6575fae8c
dominatorVJ <1vishaljee@gmail.com>
2023-08-28 21:37:21 +0530

    deploying 2.1

--- backend/key.js
+++
@@ -1,8 +0,0 @@
-const JWT_KEY = "4rgsgdffhrfdggete5575986789thgf";
-const pass = "oowecvgfgtmyvlcf";
-const db_link =
-  "mongodb+srv://1vishaljee:1vishaljee@cluster0.yrcgkie.mongodb.net/?retryWrites=true&w=majority";
-const flightAPI = "ae65c6c19fmsh43997907fbf7f73p171289jsn5568b4f90170";
-const trainAPI = "c7f01c3b62msh8fa8a769a9bb44bp1e60d9jsn05992acbe3d7";
-const hotelAPI = "c7f01c3b62msh8fa8a769a9bb44bp1e60d9jsn05992acbe3d7";
-module.exports = { JWT_KEY, pass, db_link, flightAPI, trainAPI, hotelAPI };
